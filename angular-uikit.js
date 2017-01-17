e913b752f9ed6f0c9d670ee0feed939fd9e59177
Manuel Larsen <manuel.larsen@holonix.it>
2017-01-17 16:31:15 +0100

    Added ukNgNotAllowArrayDuplicate

--- angular-uikit.js
+++ angular-uikit.js
@@ -484,4 +484,39 @@ angular.module('angularUikit', [])
                 }
             }
         }
+    })
+    .directive('ukNgNotAllowArrayDuplicate', function () {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            scope: {
+                ukNgNotAllowArrayDuplicate: "=?",
+                callbackUrl: "&?" //TODO
+            },
+            link: function (scope, element, attrs, ngModelController) {
+                ngModelController.$parsers.unshift(function (value) {
+                    if(ngModelController.$modelValue!==value){
+                        var valid = !scope.ukNgNotAllowArrayDuplicate.source.some(function (elem) {
+                            var existingValue = elem[scope.ukNgNotAllowArrayDuplicate.attribute];
+                            if (existingValue && value)
+                                return existingValue.toUpperCase() === value.toUpperCase();
+                            return false;
+                        });
+
+                        ngModelController.$setValidity('duplicate', valid);
+                        return valid ? value : undefined;
+                    }
+                });
+
+                ngModelController.$formatters.unshift(function (value) {
+                    ngModelController.$setValidity('duplicate', !scope.ukNgNotAllowArrayDuplicate.source.some(function (elem) {
+                        var existingValue = elem[scope.ukNgNotAllowArrayDuplicate.attribute];
+                        if (existingValue && value)
+                            return existingValue.toUpperCase() === value.toUpperCase();
+                        return false;
+                    }));
+                    return value;
+                });
+            }
+        };
     });
