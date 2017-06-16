dcf4bff8c6309b29969c055ace7ecc086cd4427b
Manuel Larsen <manuel.larsen@holonix.it>
2017-06-16 17:33:18 +0200

    bugfix pagination

--- src/angular-uikit-pagination.js
+++ src/angular-uikit-pagination.js
@@ -1,38 +1,39 @@
 export default function ukNgPagination() {
-        return {
-            restrict: 'AE',
-            scope: {
-                listSize: '=',
-                pageSize: '=',
-                onPageChange: '&'
-            },
-            link: function (scope, element, attrs) {
-                var ukPaginationOptions = {
-                    items: scope.listSize,
-                    itemsOnPage: scope.pageSize,
-                    currentPage: 0,
-                    displayedPages: 3,
-                    edges: 1
-                };
+    return {
+        restrict: 'AE',
+        scope: {
+            listSize: '=',
+            pageSize: '=',
+            currentPage: '?=',
+            onPageChange: '&'
+        },
+        link: function (scope, element, attrs) {
+            var ukPaginationOptions = {
+                items: scope.listSize,
+                itemsOnPage: scope.pageSize,
+                currentPage: scope.currentPage ? scope.currentPage : 0,
+                displayedPages: 3,
+                edges: 1
+            };
 
-                var pagination = UIkit.pagination(element, ukPaginationOptions);
-                window.pag = pagination;
+            var pagination = UIkit.pagination(element, ukPaginationOptions);
+            window.pag = pagination;
 
-                element.on('select.uk.pagination', function (e, pageIndex) {
-                    scope.onPageChange({$page: pageIndex});
-                });
+            element.on('select.uk.pagination', function (e, pageIndex) {
+                scope.onPageChange({$page: pageIndex});
+            });
 
-                scope.$watch('scope.listSize', function () {
-                    pagination.options.items = scope.listSize;
-                    pagination.pages = Math.ceil(scope.listSize / scope.pageSize);
-                    pagination.render();
-                });
+            scope.$watch('scope.listSize', function () {
+                pagination.options.items = scope.listSize;
+                pagination.pages = Math.ceil(scope.listSize / scope.pageSize);
+                pagination.render();
+            });
 
-                scope.$watch('scope.pageSize', function () {
-                    pagination.options.itemsOnPage = scope.pageSize;
-                    pagination.pages = Math.ceil(scope.listSize / scope.pageSize);
-                    pagination.render();
-                });
-            }
-        };
-    }
+            scope.$watch('scope.pageSize', function () {
+                pagination.options.itemsOnPage = scope.pageSize;
+                pagination.pages = Math.ceil(scope.listSize / scope.pageSize);
+                pagination.render();
+            });
+        }
+    };
+}
