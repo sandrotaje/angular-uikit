c1fb0b1b1920ab7ba4f3dd5ed15432303289b3e1
Sandro Tajè <sandro.taje@holonix.it>
2018-06-04 10:26:37 +0200

    isolate form fix

--- src/angular-uikit.js
+++ src/angular-uikit.js
@@ -24,29 +24,27 @@ angular.module('angularUikit', [])
                 if (!ctrl) {
                     return;
                 }
-    
+
                 // Do a copy of the controller
                 var ctrlCopy = {};
                 angular.copy(ctrl, ctrlCopy);
-    
+
                 // Get the parent of the form
                 var parent = elm.parent().controller('form');
                 // Remove parent link to the controller
                 parent.$removeControl(ctrl);
-    
+
                 // Replace form controller with a "isolated form"
-                var isolatedFormCtrl = {
-                    $setValidity: function (validationToken, isValid, control) {
-                        ctrlCopy.$setValidity(validationToken, isValid, control);
-                        parent.$setValidity(validationToken, true, ctrl);
-                    },
-                    $setDirty: function () {
-                        elm.removeClass('ng-pristine').addClass('ng-dirty');
-                        ctrl.$dirty = true;
-                        ctrl.$pristine = false;
-                    },
+                ctrl.$setValidity = function (validationToken, isValid, control) {
+                    ctrlCopy.$setValidity(validationToken, isValid, control);
+                    parent.$setValidity(validationToken, true, ctrl);
                 };
-                angular.extend(ctrl, isolatedFormCtrl);
+                ctrl.$setDirty = function () {
+                    elm.removeClass('ng-pristine').addClass('ng-dirty');
+                    ctrl.$dirty = true;
+                    ctrl.$pristine = false;
+                };
             }
         };
-    }]);;
+    }]);
+;
